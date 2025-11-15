a764a84f368436b5f8fa38750a76b552993d3e81
Samuel Byalugaba <sammyjerry5@gmail.com>
2025-11-15 15:11:58 +0000

    I have uploaded this logo in the root directory, I want it to be used as

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,27 +1,15 @@
-import type { SVGProps } from "react";
+import Image from 'next/image';
+import type { SVGProps } from 'react';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: Omit<SVGProps<SVGSVGElement>, 'width' | 'height'> & { width?: number, height?: number }) {
+  const { width = 28, height = 28, ...rest } = props;
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M21.3,12.2a6.3,6.3,0,0,0-5.2-5.4" />
-      <path d="M3.9,8.5a6.3,6.3,0,0,0,0,7" />
-      <path d="M11.6,2.7a6.3,6.3,0,0,0-7,0" />
-      <path d="M12.4,21.3a6.3,6.3,0,0,0,7,0" />
-      <path d="M17.5,3.9a6.3,6.3,0,0,0-7,0" />
-      <path d="M8.5,20.1a6.3,6.3,0,0,0,7,0" />
-      <path d="M2.7,12.4a6.3,6.3,0,0,0,5.4,5.2" />
-      <path d="M20.1,8.5a6.3,6.3,0,0,0,0-7" />
-      <line x1="12" y1="8" x2="12" y2="16" />
-      <line x1="8" y1="12" x2="16" y2="12" />
-    </svg>
+    <Image
+      src="/TibaFLow.png"
+      alt="TibaFlow Logo"
+      width={width}
+      height={height}
+      className={props.className}
+    />
   );
 }
